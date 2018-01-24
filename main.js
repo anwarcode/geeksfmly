fc3937b923f8c9ea61a098ed3c64d946fb3ef23a
anwarcode <21023424+anwarcode@users.noreply.github.com>
2018-01-24 13:15:31 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+$(function() {  
+    $("body").niceScroll();
+});
+
+$("body").niceScroll({
+cursorcolor:"#3a7fff",
+cursorwidth:"10px",
+cursorborder:'none',
+cursorborderradius:2,
+autohidemode:'false',
+scrollspeed: 80
+});
